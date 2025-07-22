da854188469b1ada97a68d4062cb9f5dbfa7057d
Atif Ali <atifshoukatali@yahoo.com>
2025-07-21 20:19:00 -0700

    include add column logic

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import PlusIcon from "../icons/PlusIcon"
+import type { Column } from "../types";
+
 function KanbanBoard() {
+    const [columns, setColumns] = useState<Column[]>([]);
+
+    function createNewColumn() {
+        const columnToAdd: Column = {
+            id: Date.now(),
+            title: `Column ${columns.length + 1}`,
+        };
+        setColumns([...columns, columnToAdd]);
+    }
+
     return (
-        <div>KanbanBoard</div>
+        <div className="m-auto flex min-h-screen w-full items-center
+            overflow-x-auto overflow-y-hidden px-[40px]">
+            <div className="m-auto">
+
+                <button className="h-[60px] w-[350px] min-w-[350px] 
+                    cursor-pointer rounded-lg bg-mainBackgroundColor 
+                    border-2 border-columnBackgroundColor p-4 
+                    ring-rose-500 hover:ring-2 flex gap-2"
+                    onClick={createNewColumn}>
+                    <PlusIcon />
+                    Add Column
+                </button>
+            </div>
+        </div>
     )
 }
 
