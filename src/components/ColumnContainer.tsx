9d2ba52472c130c1682b3e91e4f903a68ebd93fe
Atif Ali <atifshoukatali@yahoo.com>
2025-07-23 17:08:27 -0700

    make columns draggable

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,5 +1,7 @@
+import { useSortable } from "@dnd-kit/sortable";
 import TrashIcon from "../icons/TrashIcon";
 import type { Column, Id } from "../types";
+import { CSS } from "@dnd-kit/utilities"
 
 interface Props {
     column: Column;
@@ -8,12 +10,26 @@ interface Props {
 
 function ColumnContainer(props: Props) {
     const { column, deleteColumn } = props;
+    const { setNodeRef, attributes, listeners,
+        transform, transition } = useSortable({
+            id: column.id,
+            data: {
+                type: "Column",
+                column,
+            },
+        });
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform),
+    }
+
     return (
-        <div className="bg-gray-800 w-[350px] h-[500px] 
-            max-h-[500px] rounded-md flex flex-col">
-            <div className="bg-gray-950 text-md h-[60px] cursor-grab
-                rounded-md rounded-b-none p-3 font-bold 
-                border-gray-800 border-4 flex items-center 
+        <div ref={setNodeRef} style={style} className="bg-gray-800 
+            w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+            <div {...attributes} {...listeners} className="bg-gray-950 
+                text-md h-[60px] cursor-grab rounded-md rounded-b-none 
+                p-3 font-bold border-gray-800 border-4 flex items-center 
                 justify-between">
                 <div className="flex gap-2">
                     <div className="flex justify-center items-center 
@@ -24,7 +40,8 @@ function ColumnContainer(props: Props) {
                 </div>
                 <button onClick={() => deleteColumn(column.id)}
                     className="stroke-gray-500 
-                    hover:stroke-white hover:bg-gray-800 rounded px-1 py-2">
+                    hover:stroke-white hover:bg-gray-800 rounded 
+                    px-1 py-2">
                     <TrashIcon />
                 </button>
             </div>
