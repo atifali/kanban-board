da6f3c469a331d79cac99da3f2b83d336328a79b
Atif Ali <atifshoukatali@yahoo.com>
2025-07-26 12:14:21 -0700

    add task remove button

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import TrashIcon from "../icons/TrashIcon";
 import type { Task } from "../types"
 
 interface Props {
@@ -6,11 +8,23 @@ interface Props {
 
 function TaskCard(props: Props) {
     const { task } = props;
+    const [mouseIsOver, setMouseIsOver] = useState(false);
     return (
         <div className="bg-gray-950 p-2.5 h-[100px] min-h-[100px] 
             items-center flex text-left rounded-xl hover:ring-2 
-            hover:ring-inset hover:ring-rose-500">
+            hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+            onMouseEnter={() => {
+                setMouseIsOver(true);
+            }}
+            onMouseLeave={() => {
+                setMouseIsOver(false);
+            }}
+        >
             {task.content}
+            {mouseIsOver && <button className="stroke-white absolute right-4 top-1/2
+            -translate-y-1/2 bg-gray-800 p-2 rounded">
+                <TrashIcon />
+            </button>}
         </div>
     )
 }
