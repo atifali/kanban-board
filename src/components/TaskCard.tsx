eae42c6179c1f7c0383cd122a3bca248310912a0
Atif Ali <atifshoukatali@yahoo.com>
2025-07-27 15:10:22 -0700

    introduce edit mode for tasks

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -10,10 +10,24 @@ interface Props {
 function TaskCard(props: Props) {
     const { task, deleteTask } = props;
     const [mouseIsOver, setMouseIsOver] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+
+    function toggleEditMode() {
+        setEditMode((prev) => !prev);
+        setMouseIsOver(false);
+    }
+
+    if (editMode) {
+        return (
+            <>Edit Mode</>
+        );
+    }
+
     return (
         <div className="bg-gray-950 p-2.5 h-[100px] min-h-[100px] 
             items-center flex text-left rounded-xl hover:ring-2 
             hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+            onClick={toggleEditMode}
             onMouseEnter={() => {
                 setMouseIsOver(true);
             }}
