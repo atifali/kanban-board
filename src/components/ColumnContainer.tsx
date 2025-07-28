884a8904ae8fcebe1f7a37bbb21ac953bf4cf0d9
Atif Ali <atifshoukatali@yahoo.com>
2025-07-28 15:06:55 -0700

    add tasksIds as items

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,8 +1,8 @@
-import { useSortable } from "@dnd-kit/sortable";
+import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import TrashIcon from "../icons/TrashIcon";
 import type { Column, Id, Task } from "../types";
 import { CSS } from "@dnd-kit/utilities"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import TaskCard from "./TaskCard";
 
@@ -20,6 +20,9 @@ function ColumnContainer(props: Props) {
     const { column, deleteColumn, updateColumn, createTask,
         deleteTask, updateTask, tasks } = props;
     const [editMode, setEditMode] = useState(false);
+    const tasksIds = useMemo(() => {
+        return tasks.map(task => task.id);
+    }, [tasks]);
 
     const { setNodeRef, attributes, listeners,
         transform, transition, isDragging } = useSortable({
@@ -91,13 +94,15 @@ function ColumnContainer(props: Props) {
             </div>
             <div className="flex flex-grow flex-col gap-4 p-2 
                 overflow-x-hidden overflow-y-auto">
-                {tasks.map(task => (
-                    <TaskCard key={task.id}
-                        task={task}
-                        deleteTask={deleteTask}
-                        updateTask={updateTask}
-                    />
-                ))}
+                <SortableContext items={tasksIds}>
+                    {tasks.map(task => (
+                        <TaskCard key={task.id}
+                            task={task}
+                            deleteTask={deleteTask}
+                            updateTask={updateTask}
+                        />
+                    ))}
+                </SortableContext>
             </div>
             <button className="flex gap-2 items-center border-gray-800
                 border rounded-md p-4 border-x-gray-800 
